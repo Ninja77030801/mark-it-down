d34b28272f10117575f9a2d95c4f7cbf58dd5699
ninja77030801 <n77030@outlook.com>
2022-11-13 11:03:28 +0530

    Added files

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Navbar, Button, Alignment } from '@blueprintjs/core';
+import SimpleMDE from "react-simplemde-editor";
+import 'easymde/dist/easymde.min.css';
+import './App.css';
+
+function App() {
+  const [code, setCode] = useState("# Hello World! \n You can save your progress on the browser, or you can load your recent work.");
+  const saveProgress = () => {
+    localStorage.setItem("recentCode", code);
+  };
+  const loadProgress = () => {
+    try {
+      var recentCode = localStorage.getItem("recentCode");
+      if(recentCode !== null){
+        setCode(recentCode)
+      }
+    }
+    catch(error: any){
+     alert(error.message);
+    }
+  };
+  const saveFile = () => {
+    var blob = new Blob([code], {type: "octet/stream"});
+    var url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = prompt("Enter the name of your file")+".md";
+    a.click();
+  };
+  return (
+    <div className="App">
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+          <h3>Mark it down</h3>
+          <Navbar.Divider />
+          <Button icon="download" text="Save Progress" onClick={saveProgress} className="bp4-minimal" />
+          <Button icon="download" onClick={saveFile} text="Save file" className="bp4-minimal" />
+          <Button icon="export" text="Export as HTML" className="bp4-minimal" />
+          <Button icon="upload" text="Load Progress" onClick={loadProgress} className="bp4-minimal" />
+        </Navbar.Group>
+      </Navbar>
+      <SimpleMDE value={code} onChange={(value) => setCode(value)} />
+    </div>
+  );
+}
+
+export default App;
